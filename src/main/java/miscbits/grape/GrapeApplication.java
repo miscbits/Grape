fab3a86f19b473e8b5f89d10debd5f0da6b03245
miscbits <willow6786@gmail.com>
2018-02-06 16:54:21 -0500

    initial commit

---
+++ src/main/java/miscbits/grape/GrapeApplication.java
@@ -0,0 +1,12 @@
+package miscbits.grape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapeApplication.class, args);
+	}
+}
